d28b33c9586a1861e376c5538f87ecfb845bea5f
sssa2000 <sssa2000@qq.com>
2016-11-11 17:55:16 +0800

    first commit

---
+++ first_sfm/first_sfm/first_sfm.cpp
@@ -0,0 +1,23 @@
+// first_sfm.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "opencv/highgui.h"
+
+#pragma comment(lib,"opencv_core310d.lib")
+#pragma comment(lib,"opencv_ml310d.lib")
+#pragma comment(lib,"opencv_highgui310d.lib")
+#pragma comment(lib,"opencv_imgcodecs310d.lib")
+
+
+int main()
+{
+	IplImage* img = cvLoadImage("../media/Angry-baby-owl.jpg");
+	//cvNamedWindow("window1", CV_WINDOW_AUTOSIZE);
+	cvShowImage("test2", img);
+	cvWaitKey(0);
+	cvReleaseImage(&img);
+	//cvDestroyWindow("window1");
+	return 0;
+}
+
